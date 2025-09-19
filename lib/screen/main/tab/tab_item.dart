e38183af1487c270603982426e1c0d2ac25964b6
영중 <yungjoong.song@gmail.com>
2025-09-19 20:57:06 +0900

    - BottomNavigation  수정하기

--- lib/screen/main/tab/tab_item.dart
+++ lib/screen/main/tab/tab_item.dart
@@ -1,13 +1,17 @@
 import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/screen/main/tab/all/f_all.dart';
+import 'package:fast_app_base/screen/main/tab/benefit/f_benefit.dart';
 import 'package:fast_app_base/screen/main/tab/home/f_home.dart';
+import 'package:fast_app_base/screen/main/tab/ttospay/f_ttospay.dart';
+import 'package:fast_app_base/screen/main/tab/stock/f_stock.dart';
 import 'package:flutter/material.dart';
 
 enum TabItem {
   home(Icons.home, '홈', HomeFragment()),
-  benefit(Icons.star, '혜택', HomeFragment()),
-  ttosspay(Icons.payment, '토스페이', HomeFragment()),
-  stock(Icons.candlestick_chart, '주식', HomeFragment()),
-  all(Icons.menu, '전체', HomeFragment());
+  benefit(Icons.star, '혜택', BenefitFragment()),
+  ttosspay(Icons.payment, '토스페이', TtospayFragment()),
+  stock(Icons.candlestick_chart, '주식', StockFragment()),
+  all(Icons.menu, '전체', AllFragment());
 
   final IconData activeIcon;
   final IconData inActiveIcon;
