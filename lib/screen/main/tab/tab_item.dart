26d41b8b4018d6839e3c1be7e7e69e90083464b9
영중 <yungjoong.song@gmail.com>
2025-09-19 20:45:28 +0900

    - 하단 메뉴 추가

--- lib/screen/main/tab/tab_item.dart
+++ lib/screen/main/tab/tab_item.dart
@@ -1,11 +1,13 @@
 import 'package:fast_app_base/common/common.dart';
-import 'package:fast_app_base/screen/main/tab/favorite/f_favorite.dart';
 import 'package:fast_app_base/screen/main/tab/home/f_home.dart';
 import 'package:flutter/material.dart';
 
 enum TabItem {
   home(Icons.home, '홈', HomeFragment()),
-  favorite(Icons.star, '즐겨찾기', FavoriteFragment(isShowBackButton: false));
+  benefit(Icons.star, '혜택', HomeFragment()),
+  ttosspay(Icons.payment, '토스페이', HomeFragment()),
+  stock(Icons.candlestick_chart, '주식', HomeFragment()),
+  all(Icons.menu, '전체', HomeFragment());
 
   final IconData activeIcon;
   final IconData inActiveIcon;
